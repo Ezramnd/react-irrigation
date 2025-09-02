ec37b5ccbf2c599cf8d70527915d8d4658fc7b9b
Azis Maulana <azismaulana@apps.ipb.ac.id>
2025-09-02 15:05:20 +0700

    perbaikan login

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -78,7 +78,7 @@ const DataTable = () => {
   return (
     <div className="bg-white p-6 rounded-2xl shadow-lg">
       <div className="flex justify-between items-center mb-4 border-b pb-4">
-        <h2 className="text-xl font-bold text-gray-800">Log Data Sensor</h2>
+        <h2 className="text-xl font-bold text-gray-800">Data Green House</h2>
         <button onClick={handleDownloadCSV} className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 font-semibold text-sm flex items-center space-x-2">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
           <span>Download CSV</span>
